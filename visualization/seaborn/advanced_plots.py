c62a1dee9d37ee8daae4a840d601c8d458f2a38b
Manuel Morini <morinimanuel@gmail.com>
2020-01-02 16:37:56 +0100

    docs on seaborn

--- visualization/seaborn/advanced_plots.py
+++ visualization/seaborn/advanced_plots.py
@@ -18,30 +18,57 @@ df = pd.DataFrame({
 
 fig = plt.figure()
 # Setting styles:
-sns.set_style("darkgrid")
-sns.set_palette("pastel")
+sns.set_style("whitegrid")
+#Seaborn has six variations of its default color palette: deep, muted, pastel, bright, dark, and colorblind.
+sns.set_palette("deep")
+'''
+In addition to the default palette and its variations, 
+Seaborn also allows the use of Color Brewer palettes. 
+Color Brewer is the name of a set of color palettes inspired by 
+the research of cartographer Cindy Brewer. 
+The color palettes are specifically chosen to be easy to interpret
+when used to represent ordered categories. 
+They are also colorblind accessible, as each color differs from its neighbors 
+in lightness or tint.
+To use, pass the name of any Color Brewer palette directly to any of the color functions:
+    custom_palette = sns.color_palette("Paired", 9)
+    sns.palplot(custom_palette)
+'''
+#sns.despine()
 
 #Creating a barplot
-fig.add_subplot(4,1,1)
+fig.add_subplot(2,1,1)
+sns.set_context("poster")
 sns.barplot(data=df,  x="label", y="value")
 
 
-fig.add_subplot(4,1,2)
-
+fig.add_subplot(2,1,2)
+sns.set_context("paper")
 sns.kdeplot(set_one, shade=True)
 sns.kdeplot(set_two, shade=True)
 sns.kdeplot(set_three, shade=True)
 sns.kdeplot(set_four, shade=True)
 
 plt.legend(['Set One','Set Two','Set Three','Set Four'])
+# plt.show()
 
-
-fig.add_subplot(4,2,7)
+#Box Plot
+fig2=plt.figure(2)
+fig2.add_subplot(2,1,1)
+sns.set_context("notebook")
 sns.boxplot(data=df, x='label',y='value')
 
 #Violin plot
-fig.add_subplot(4,2,8)
+# fig2.add_subplot(4,2,8)
+fig2.add_subplot(2,1,2)
+#rc stands for run command and overrides a specific value from seaborn dictionary -> sns.plotting_context()
+sns.set_context("talk",font_scale = .5, rc={"grid.linewidth": 0.6})
 sns.violinplot(data=df, x='label',y='value')
 
+# Color Palette
+#palette = sns.color_palette("bright")
+#sns.palplot(palette)
+
+#sns.despine()
 plt.show()
 
